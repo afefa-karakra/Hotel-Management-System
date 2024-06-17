afc3e717688959802699ac9ab1324c22b317633f
AFEFA KARAKRA <karakra.afifa123@gmail.com>
2024-06-17 19:49:26 +0300

    docApi Customer

--- src/main/java/com/example/hotelmanagementsystem/Controller/CustomerController.java
+++ src/main/java/com/example/hotelmanagementsystem/Controller/CustomerController.java
@@ -6,7 +6,13 @@ import com.example.hotelmanagementsystem.Entity.Customer;
 import com.example.hotelmanagementsystem.Service.Interface.CustomerService;
 import com.example.hotelmanagementsystem.Entity.User;
 //import io.swagger.annotations.Api;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,20 +28,37 @@ import static com.example.hotelmanagementsystem.user.Role.ADMIN;
 // The @RestController annotation informs Spring that this class is a controller that should process HTTP requests
 // The @RequestMapping("/api/v1/customers") annotation maps this controller to the specified URL path,
 // which means that any requests starting with /api/v1/customers will be handled by methods in this controller
-//@Api(tags = "Customers")
+@Api(tags = "Customers")
 @RestController
 @RequestMapping("/api/v1/customers")
 public class CustomerController {
     // The @Autowired annotation tells Spring to inject the CustomerService into this controller
     // When the controller is created, Spring will create an instance of the CustomerService and assign it to this field
-    @Autowired
     private CustomerService customerService;
-    // This method returns all the customers.
-    // The @GetMapping annotation informs that this method should respond to HTTP GET requests
-    // It uses the customer service to fetch all the customers and return them
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 
     @ApiOperation(value = "Get all customers")
     @GetMapping
+    @Operation(
+            security = @SecurityRequirement(name = "token"),
+            description = "Get all customers service",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully get all customers!",
+                            content = @Content(
+                                    mediaType ="application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    value = "{\"code\" : 200, \"Status\" : \"Ok!\", \"Message\" :\"Successfully get all customers!\"}"
+                                            ),
+                                    }
+                            )
+                    )
+            }
+    )
     public ResponseEntity<List<CustomerDTO>> getAllCustomers() {
         // Calling the getAllCustomers method of the customerService
         List<CustomerDTO> cs=customerService.getAllCustomers();
@@ -53,6 +76,24 @@ public class CustomerController {
     // If the requested customer id exists, the method returns the customer, otherwise it returns HTTP status code 404
     @ApiOperation(value = "Get customer by ID")
     @GetMapping("/{id}")
+    @Operation(
+            security = @SecurityRequirement(name = "token"),
+            description = "Get customer by id service",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully get customer by id!",
+                            content = @Content(
+                                    mediaType ="application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    value = "{\"code\" : 200, \"Status\" : \"Ok!\", \"Message\" :\"Successfully get customer by id!\"}"
+                                            ),
+                                    }
+                            )
+                    )
+            }
+    )
     public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable long id, Authentication auth) {
 //        UserDetails userDetails = (UserDetails) auth.getPrincipal();
 //        User user = (User) userDetails;
@@ -78,6 +119,24 @@ public class CustomerController {
     // The @PostMapping annotation informs that this method should respond to HTTP POST requests
     @ApiOperation(value = "Create a new customer")
     @PostMapping
+    @Operation(
+            security = @SecurityRequirement(name = "token"),
+            description = "Create customer service",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Successfully create customers!",
+                            content = @Content(
+                                    mediaType ="application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    value = "{\"code\" : 201, \"Status\" : \"Created!\", \"Message\" :\"Successfully create customer!\"}"
+                                            ),
+                                    }
+                            )
+                    )
+            }
+    )
     public ResponseEntity<CustomerDTO> createCustomer(@RequestBody CustomerDTO customerDTO) {
 
         return ResponseEntity.ok(customerService.createCustomer(customerDTO));
@@ -94,6 +153,24 @@ public class CustomerController {
 //    }
     @ApiOperation(value = "Update customer")
     @PutMapping("/{id}")
+    @Operation(
+            security = @SecurityRequirement(name = "token"),
+            description = "Update customer info service",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "202",
+                            description = "Successfully Update customer info!",
+                            content = @Content(
+                                    mediaType ="application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    value = "{\"code\" : 202, \"Status\" : \"Accepted!\", \"Message\" :\"Successfully Update customer info!\"}"
+                                            ),
+                                    }
+                            )
+                    )
+            }
+    )
     public ResponseEntity<CustomerDTO> updateCustomer(@PathVariable long id, @RequestBody CustomerDTO customerDTO) {
         CustomerDTO updatedCustomer = customerService.updateCustomer(id, customerDTO);
         return ResponseEntity.ok(updatedCustomer);
@@ -103,6 +180,24 @@ public class CustomerController {
 
     @ApiOperation(value = "Delete customer by ID")
     @DeleteMapping("/{id}")
+    @Operation(
+            security = @SecurityRequirement(name = "token"),
+            description = "Delete customer service",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully delete customer!",
+                            content = @Content(
+                                    mediaType ="application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    value = "{\"code\" : 200, \"Status\" : \"Accepted!\", \"Message\" :\"Successfully delete customer!\"}"
+                                            ),
+                                    }
+                            )
+                    )
+            }
+    )
     public ResponseEntity<Void> deleteCustomer(@PathVariable long id) {
         // Calling the deleteCustomer method of the customerService with the id of the customer to delete
         // After successful deletion, it returns an HTTP 200 status code without body content
@@ -113,6 +208,24 @@ public class CustomerController {
 
     @ApiOperation(value = "find customer by name")
     @GetMapping("/name/{name}")
+    @Operation(
+            security = @SecurityRequirement(name = "token"),
+            description = "find customer by name",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Successfully find customer by name",
+                            content = @Content(
+                                    mediaType ="application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    value = "{\"code\" : 201, \"Status\" : \"Created!\", \"Message\" :\"Successfully find customer by name\"}"
+                                            ),
+                                    }
+                            )
+                    )
+            }
+    )
     public ResponseEntity<List<CustomerDTO>> findCustomerByName(@PathVariable String name) {
         List<CustomerDTO> customerList = customerService.findCustomerByName(name);
         return ResponseEntity.ok(customerList);
