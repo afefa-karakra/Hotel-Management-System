e1884024573ca51a6f6cdc6c8665cc7b52d9457d
AFEFA KARAKRA <karakra.afifa123@gmail.com>
2024-06-17 19:57:05 +0300

    docApi HousekeepingTasks

--- src/main/java/com/example/hotelmanagementsystem/Controller/HousekeepingTasksController.java
+++ src/main/java/com/example/hotelmanagementsystem/Controller/HousekeepingTasksController.java
@@ -4,6 +4,11 @@ import com.example.hotelmanagementsystem.DTO.EmployeeDTO;
 import com.example.hotelmanagementsystem.DTO.HousekeepingTasksDTO;
 import com.example.hotelmanagementsystem.Service.Interface.HousekeepingTasksService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -23,6 +28,24 @@ public class HousekeepingTasksController {
 
     @ApiOperation(value = "Get HousekeepingTasks by ID")
     @GetMapping("/{id}")
+    @Operation(
+            security = @SecurityRequirement(name = "token"),
+            description = "Get HousekeepingTasks by id service",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully get HousekeepingTasks by id!",
+                            content = @Content(
+                                    mediaType ="application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    value = "{\"code\" : 200, \"Status\" : \"Ok!\", \"Message\" :\"Successfully get HousekeepingTasks by id!\"}"
+                                            ),
+                                    }
+                            )
+                    )
+            }
+    )
     public ResponseEntity<HousekeepingTasksDTO> HousekeepingTasksById(@PathVariable long id, Authentication auth) {
 
         HousekeepingTasksDTO housekeepingTasksDTO = housekeepingTasksService.getHousekeepingTaskById(id);
@@ -32,6 +55,24 @@ public class HousekeepingTasksController {
 
     @ApiOperation(value = "Get all HousekeepingTasks")
     @GetMapping
+    @Operation(
+            security = @SecurityRequirement(name = "token"),
+            description = "Get all HousekeepingTasks service",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully get all HousekeepingTasks!",
+                            content = @Content(
+                                    mediaType ="application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    value = "{\"code\" : 200, \"Status\" : \"Ok!\", \"Message\" :\"Successfully get all HousekeepingTasks!\"}"
+                                            ),
+                                    }
+                            )
+                    )
+            }
+    )
     public ResponseEntity<List<HousekeepingTasksDTO>> getAllHousekeepingTasks (){
 
         return ResponseEntity.ok().body(housekeepingTasksService.getAllHousekeepingTask());
@@ -39,6 +80,24 @@ public class HousekeepingTasksController {
 
     @ApiOperation(value = "Create a new HousekeepingTasks")
     @PostMapping
+    @Operation(
+            security = @SecurityRequirement(name = "token"),
+            description = "Create HousekeepingTasks service",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Successfully create HousekeepingTasks!",
+                            content = @Content(
+                                    mediaType ="application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    value = "{\"code\" : 201, \"Status\" : \"Created!\", \"Message\" :\"Successfully create HousekeepingTasks!\"}"
+                                            ),
+                                    }
+                            )
+                    )
+            }
+    )
     public ResponseEntity<HousekeepingTasksDTO> createHousekeepingTasksDTO(@RequestBody HousekeepingTasksDTO housekeepingTasksDTO) {
 
         return ResponseEntity.ok(housekeepingTasksService.createHousekeepingTask(housekeepingTasksDTO));
@@ -46,6 +105,24 @@ public class HousekeepingTasksController {
 
     @ApiOperation(value = "Update HousekeepingTasks")
     @PutMapping("/{id}")
+    @Operation(
+            security = @SecurityRequirement(name = "token"),
+            description = "Update HousekeepingTasks info service",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "202",
+                            description = "Successfully Update HousekeepingTasks info!",
+                            content = @Content(
+                                    mediaType ="application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    value = "{\"code\" : 202, \"Status\" : \"Accepted!\", \"Message\" :\"Successfully Update HousekeepingTasks info!\"}"
+                                            ),
+                                    }
+                            )
+                    )
+            }
+    )
     public ResponseEntity<HousekeepingTasksDTO> updateHousekeepingTasksDTO(@PathVariable long id, @RequestBody HousekeepingTasksDTO housekeepingTasksDTO) {
         HousekeepingTasksDTO updatedHousekeepingTasks = housekeepingTasksService.updateHousekeepingTask(id , housekeepingTasksDTO);
         return ResponseEntity.ok(updatedHousekeepingTasks);
@@ -53,6 +130,24 @@ public class HousekeepingTasksController {
 
     @ApiOperation(value = "Delete HousekeepingTasks by ID")
     @DeleteMapping("/{id}")
+    @Operation(
+            security = @SecurityRequirement(name = "token"),
+            description = "Delete HousekeepingTasks service",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully delete HousekeepingTasks!",
+                            content = @Content(
+                                    mediaType ="application/json",
+                                    examples = {
+                                            @ExampleObject(
+                                                    value = "{\"code\" : 200, \"Status\" : \"Accepted!\", \"Message\" :\"Successfully delete HousekeepingTasks!\"}"
+                                            ),
+                                    }
+                            )
+                    )
+            }
+    )
     public ResponseEntity<String> deleteHousekeepingTasks(@PathVariable(name = "id") long id){
         housekeepingTasksService.deleteHousekeepingTaskById(id);
         return new ResponseEntity<>("Deleted successfully!", HttpStatus.OK);
